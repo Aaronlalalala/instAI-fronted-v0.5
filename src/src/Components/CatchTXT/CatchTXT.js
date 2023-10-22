bf24cb11b58806f274e240bae0b066470bdf0089
Aaronlalalala <9268michael@gmail.com>
2023-10-22 22:53:39 +0800

    已修改catch資料的方式 需要彥君作驗證 或者在進行直接的修改 確保功能細化

--- src/src/Components/CatchTXT/CatchTXT.js
+++ src/src/Components/CatchTXT/CatchTXT.js
@@ -1,30 +1,70 @@
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import JSZip from 'jszip';
-import './catch.css'; 
+import './catch.css';
 
 function CatchTXTimage() {
   const [images, setImages] = useState([]);
   const [downloading, setDownloading] = useState(false);
-  const [error, setError] = useState(null); 
+  const [error, setError] = useState(null);
+  const [selectedImages, setSelectedImages] = useState([]); // 用於追蹤用戶選擇的圖像
 
   const fetchDataFromAPI = async () => {
     try {
       const response = await axios.get('http://localhost:8080/api/txt2img/process');
       setImages(response.data);
-      setError(null); // 清出錯誤
+      setError(null);
     } catch (error) {
-      console.error('Error fetching data from the API:', error);
-      setError('數據獲取失敗'); // 設置錯誤處理
+      console.error('從API獲取數據時出錯:', error);
+      setError('數據獲取失敗');
     }
   };
 
-  const downloadImages = async () => {
-    if (images.length === 0) return;
+  const encodeImageAsBase64 = async (imageUrl) => {
+    try {
+      const response = await fetch(imageUrl);
+      if (response.ok) {
+        const blob = await response.blob();
+        const reader = new FileReader();
+
+        reader.onload = function () {
+          const base64String = reader.result.split(',')[1];
+          downloadSingleImage(base64String, images.length);
+        };
+
+        reader.readAsDataURL(blob);
+      } else {
+        console.error('獲取圖像時出錯。');
+      }
+    } catch (error) {
+      console.error('發生錯誤:', error);
+    }
+  };
+
+  const downloadSingleImage = (base64, index) => {
+    const newImages = [...images];
+    newImages[index] = base64;
+    setImages(newImages);
+  };
+
+  // 處理用戶選擇圖像的函數
+  const handleImageSelect = (index) => {
+    if (selectedImages.includes(index)) {
+      // 如果已選擇，從列表中刪除
+      const newSelectedImages = selectedImages.filter((selected) => selected !== index);
+      setSelectedImages(newSelectedImages);
+    } else {
+      // 如果未選擇，添加到列表中
+      setSelectedImages([...selectedImages, index]);
+    }
+  };
+
+  const downloadSelectedImages = async () => {
+    if (selectedImages.length === 0) return;
     const zip = new JSZip();
 
-    images.forEach((base64, index) => {
+    selectedImages.forEach((index) => {
+      const base64 = images[index];
       const fileName = `image_${index + 1}.jpg`;
       zip.file(fileName, base64, { base64: true });
     });
@@ -33,29 +73,30 @@ function CatchTXTimage() {
       const url = window.URL.createObjectURL(content);
       const a = document.createElement('a');
       a.href = url;
-      a.download = 'images.zip';
+      a.download = 'selected_images.zip';
       a.click();
       window.URL.revokeObjectURL(url);
     });
   };
 
-  const downloadSingleImage = (base64, index) => {
-    const link = document.createElement('a');
-    link.href = base64;
-    link.download = `image_${index + 1}.jpg`;
-    link.click();
-  };
+  useEffect(() => {
+    // 組件掛載時獲取數據
+    fetchDataFromAPI();
+  }, []); // 空的依賴數組確保此代碼只執行一次。
 
   return (
     <div className="catch-container">
-      <button onClick={fetchDataFromAPI}>拽取</button>
-      <button onClick={downloadImages}>壓縮檔案</button>
-      {error && <p className="error-message">{error}</p>} {/* 顯示錯誤 */}
+      <button onClick={downloadSelectedImages}>壓縮所選圖像</button>
+      {error && <p className="error-message">{error}</p>}
       <div className="image-container">
         {images.map((base64, index) => (
           <div key={index} className="image-item">
-            <img src={base64} alt={`Image ${index}`} loading="lazy" />
-            <button onClick={() => downloadSingleImage(base64, index)}>下載</button>
+            <img src={base64} alt={`圖像 ${index}`} loading="lazy" />
+            <input
+              type="checkbox"
+              checked={selectedImages.includes(index)}
+              onChange={() => handleImageSelect(index)}
+            />
           </div>
         ))}
       </div>
