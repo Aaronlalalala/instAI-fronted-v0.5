b01eac2ae564f84730c4037365fef2b9cccb3c4f
Aaronlalalala <9268michael@gmail.com>
2023-11-18 00:41:24 +0800

    optimize the page of step

--- src/src/Nevigation/Step1/Step.js
+++ src/src/Nevigation/Step1/Step.js
@@ -6,71 +6,70 @@ import { NavLink } from 'react-router-dom';
 function Step() {
   return (
     <div className="app">
-      <header id="nav">
-        <div id="logo">
-          <img src={logo} alt="Your Logo" />
-        </div>
-        <div id="allProjects">
+      <header className="nav">
+          <img src={logo} className='logo' alt="Your Logo" />
+        <div className="allProjects">
           <div style={{ position: "relative", left: "250px", fontWeight: "bold" }}>All Projects</div>
         </div>
-        <div className="rectangle"></div>
+        <div className="stepRectangle"></div>
       </header>
       
-      <header id="subNav">
+      <header className="subNav">
         Traffic cone ...
       </header>
 
       <div className="circles">
-        <div className="circle"></div>
-        <div className="circle2"></div>
-        <div className="circle3"></div>
-        <div className="circle4"></div>
-        <div className="circle5"></div>
+        <div className="circleNo1"></div>
+        <div className="circleNo2"></div>
+        <div className="circleNo3"></div>
+        <div className="circleNo4"></div>
+        <div className="circleNo5"></div>
       </div>
 
-      <nav id="secondNav">
+      <nav className="secondNav">
         <ul>
-          <li>1. Upload training data</li>
-          <li>2. Provide your model training requirements</li>
-          <li>3. Confirm data and requirements</li>
-          <li>4. Train your AI model</li>
-          <li>5. Download AI model</li>
+          <li>Steps</li>
+          <li>1.Upload training data</li>
+          <li>2.Provide your model training requirements</li>
+          <li>3.Confirm data and requirements</li>
+          <li>4.Train your AI model</li>
+          <li>5.Download AI model</li>
         </ul>
       </nav>
 
-      <div className="frame">
+      <div className="frame1">
         <ul>
           <li>Upload training data</li>
           <li>Upload the image data you wish to use to train your style model</li>
         </ul>
-        <NavLink to='/Download2'><button className="upload-button">Upload</button></NavLink>
+        <NavLink to='/Download2'><button className="upload-buttonNo1">Upload</button></NavLink>
       </div>
 
-      <div className="frame2">
+      <div className="frameNo2 ">
         <ul>
           <li>Provide your training requirements</li>
           <li>Tell us your specific needs for AI model training</li>
         </ul>
-        <button className="upload-button2">Fill out the form</button>
+        <button className="upload-buttonNo2" >Fill out the form</button>
       </div>
 
-      <div className="frame3">
+      <div className="frameNo3">
         <ul>
           <li>Confirm data and requirements</li>
           <li>Tell your needs for AI model training</li>
         </ul>
-        <button className="upload-button3">Confirm data</button>
-        <button className="upload-button4">Confirm requirements</button>
+        <button className="upload-buttonNo3">Confirm data</button>
+        <button className="upload-buttonNo4">Confirm requirements</button>
       </div>
 
-      <div className="frame4">
+      <div className="frameNo4">
         <ul>
           <li>Training your AI model</li>
           <li>You haven't submitted data yet</li>
         </ul>
       </div>
 
-      <div className="frame5">
+      <div className="frameNo5">
         <ul>
           <li>Download AI model</li>
           <li>No model available for download</li>
